2e6197099ee2e72df28ddb7ce957180a98171ac2
YAMATO Keiichi <ymtk172@gmail.com>
2020-04-23 21:06:52 +0900

    refactor

--- src/main/java/com/yamalc/ytmp/userapi/UserApiServer.kt
+++ src/main/java/com/yamalc/ytmp/userapi/UserApiServer.kt
@@ -1,7 +1,6 @@
 package com.yamalc.ytmp.userapi
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
@@ -10,7 +9,5 @@ import org.springframework.context.annotation.ComponentScan
 class UserApiServer
 
 fun main(args: Array<String>) {
-//	val userClient = UserApiClient.create("localhost", 8081)
-//	userClient.authenticate("user1", "password1")
     runApplication<UserApiServer>(*args)
 }
