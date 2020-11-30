0808880716aedb33080f47dc9e62864bcf23fd47
yamato.keiichi <yamato.keiichi@tis.co.jp>
2020-11-30 20:05:53 +0900

    added bean validation at grpc, modified to request multiple

--- src/main/java/com/yamalc/ytmp/userapi/service/UserServiceImpl.kt
+++ src/main/java/com/yamalc/ytmp/userapi/service/UserServiceImpl.kt
@@ -2,13 +2,19 @@ package com.yamalc.ytmp.userapi.service
 
 import com.yamalc.ytmp.grpc.user.*
 import com.yamalc.ytmp.userapi.mapper.UsersMapper
+import io.grpc.Status
+import io.grpc.protobuf.ProtoUtils
 import io.grpc.stub.StreamObserver
 import org.lognet.springboot.grpc.GRpcService
+import org.springframework.beans.factory.annotation.Value
 import java.io.IOException
 import java.util.logging.Logger
+import javax.validation.ConstraintViolation
+import javax.validation.Validator
 
 @GRpcService
-class UserServiceImpl(private val usersMapper: UsersMapper) : UserGrpc.UserImplBase() {
+class UserServiceImpl(private val usersMapper: UsersMapper, val validator: Validator) : UserGrpc.UserImplBase() {
+
     private var logger: Logger = Logger.getLogger(javaClass.name)
     override fun getUserInfo(request: UserIdRequest,
                              responseObserver: StreamObserver<UserInfoResponse>) {
@@ -36,23 +42,51 @@ class UserServiceImpl(private val usersMapper: UsersMapper) : UserGrpc.UserImplB
         responseObserver.onCompleted()
     }
 
+    @Value("#{\${grpcprops.UserInfo}}")
+    val userInfoRequestKeyMap: Map<String, Int>? = null
+
     override fun registerUserInfo(request: UserInfoRequest,
                                   responseObserver: StreamObserver<RegisterUserInfoResponse>) {
-        logger.info(String.format("request: id = %s", request.id))
-        val resultType = try {
-            val result: Int = usersMapper.insert(request.id, request.displayName)
-            println(result)
-            if (result != 0) {
-                ResultType.SUCCESS
-            } else {
-                ResultType.ALREADY_EXISTS
+        request.userInfoList.forEachIndexed { index, userInfo ->
+            val constraintViolations: Set<ConstraintViolation<UserInfo>> = validator.validate<UserInfo>(userInfo)
+            if(constraintViolations.isNotEmpty()) {
+                val errorDetailBuilder = ErrorDetail.newBuilder()
+                        .setCode(ErrorCode.VALIDATION_ERROR)
+                        .setMessage("Validation failed.")
+                constraintViolations.forEach {
+                    val errorInfo = ErrorInfo.newBuilder()
+                            .setBusinessErrorCode("VALIDATION_FAILED")
+                            .setErrorLineNumber(index.toString())
+                            .setErrorField(userInfoRequestKeyMap!!.getOrElse(it.propertyPath.toString()) { 0 }) //定義誤りで見つからなければ0を入れる
+                            .setErrorDescription(it.message)
+                            .build()
+                    errorDetailBuilder.addErrorInfo(errorInfo)
+                }
+                val metadata = io.grpc.Metadata()
+                val errorDetail = errorDetailBuilder.build()
+                val key: io.grpc.Metadata.Key<ErrorDetail> = ProtoUtils.keyForProto(errorDetail)
+                metadata.put(key, errorDetail)
+                val e = Status.INTERNAL
+                        .withDescription("Validation failed occurred.")
+                        .asRuntimeException(metadata)
+                responseObserver.onError(e)
+                return
             }
-        } catch (e: IOException) {
-            ResultType.FAILURE
+            logger.info(String.format("... Inserting request: id = %s", userInfo.id))
+            try {
+                usersMapper.insert(userInfo.id, userInfo.displayName)
+            } catch (e: IOException) {
+                ResultType.FAILURE
+                val e2 = Status.UNAVAILABLE
+                        .withDescription("IOException occurred. DB might not be available.")
+                        .asRuntimeException()
+                responseObserver.onError(e2)
+                return
+            }
         }
         val userInfoResponse = RegisterUserInfoResponse
                 .newBuilder()
-                .setResult(resultType)
+                .setResult(ResultType.SUCCESS)
                 .build()
         responseObserver.onNext(userInfoResponse)
         responseObserver.onCompleted()
